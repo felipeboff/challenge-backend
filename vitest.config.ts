a44bf746deb6177ea3f60fa72037ad9ce0b131b2
felipeboff <felipe.boff3@gmail.com>
2025-12-29 23:24:55 -0300

    chore: update Vitest configuration to disable file parallelism and exclude test files from coverage reports for improved testing accuracy

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
     environment: "node",
     setupFiles: ["./src/tests/setup.ts"],
     testTimeout: 10000,
+    fileParallelism: false,
+    coverage: {
+      exclude: ["src/tests/**", "**/*.test.ts", "**/*.spec.ts"],
+    },
   },
 });
